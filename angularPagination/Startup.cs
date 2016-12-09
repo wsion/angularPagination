90639dbe8d23f78e92b881644dcd93fe3e06463f
wsion <waychanmail@gmail.com>
2016-12-09 11:24:50 +0800

    no message

---
+++ angularPagination/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(angularPagination.Startup))]
+namespace angularPagination
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
